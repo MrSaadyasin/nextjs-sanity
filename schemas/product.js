88e1e47eae5bd41eb36dc8530b8f8ded1995f201
MrSaadyasin <saad.yasin3434@gmail.com>
2024-04-04 23:26:14 +0500

    added

---
+++ schemas/product.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'products',
+    type: 'document',
+    title: 'Products',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Image'
+      },
+      {
+        name: 'description',
+        type: 'string',
+        title: 'description'
+      }
+    ]
+  }
